1a2302346c54b6af1d8c855624d5a56c74f8397f
atbee <atb@odds.team>
2021-04-24 13:55:23 +0700

    Initial first dag :bee:

---
+++ dags/user_processing.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+
+default_args = {
+    'start_date': datetime(2021, 1, 1),
+}
+with DAG(
+    'user_processing',
+    schedule_interval='@daily',
+    default_args=default_args,
+    catchup=False,
+) as dag:
+    start = DummyOperator(task_id='start')
+
+    creating_table = SqliteOperator(
+        task_id='creating_table',
+        sqlite_conn_id='db_sqlite',
+        sql='''
+            CREATE TABLE users (
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL
+            )
+        ''',
+    )
+
+    end = DummyOperator(task_id='end')
+
+    start >> creating_table >> end
