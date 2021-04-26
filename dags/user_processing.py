6d20097d29e43289a4e01b3861d99df8df6ffd1b
atbee <atb@odds.team>
2021-04-26 23:22:28 +0700

    Fix typo task_ids :bee:

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -12,19 +12,21 @@ from pandas import json_normalize
 
 
 def _processing_user(ti):
-    users = ti.xcom_pull(task_id=['extracting_user'])
+    users = ti.xcom_pull(task_ids=['extracting_user'])
     if not len(users) or 'results' not in users[0]:
         raise ValueError('User is empty!')
 
     user = users[0]['results'][0]
-    processed_user = json_normalize({
-        'email': user['email'],
-        'firstname': user['name']['first'],
-        'lastname': user['name']['last'],
-        'country': user['location']['country'],
-        'username': user['login']['username'],
-        'password': user['login']['password'],
-    })
+    processed_user = json_normalize(
+        {
+            'email': user['email'],
+            'firstname': user['name']['first'],
+            'lastname': user['name']['last'],
+            'country': user['location']['country'],
+            'username': user['login']['username'],
+            'password': user['login']['password'],
+        }
+    )
     processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
 
