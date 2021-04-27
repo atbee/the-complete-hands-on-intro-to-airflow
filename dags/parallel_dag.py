c578fbebc8ffcae62bebb16bc6f656c4f65a5d0b
atbee <atb@odds.team>
2021-04-28 00:27:34 +0700

    Minimising DAGs with SubDAGs :bee:

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -2,6 +2,9 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+
+from subdags.subdag_parallel_dag import subdag_parallel_dag
 
 default_args = {
     'start_date': datetime(2021, 1, 1),
@@ -19,14 +22,13 @@ with DAG(
         bash_command='sleep 3',
     )
 
-    task_2 = BashOperator(
-        task_id='task_2',
-        bash_command='sleep 3',
-    )
-
-    task_3 = BashOperator(
-        task_id='task_3',
-        bash_command='sleep 3',
+    processing = SubDagOperator(
+        task_id='processing_tasks',
+        subdag=subdag_parallel_dag(
+            'parallel_dag',
+            'processing_tasks',
+            default_args,
+        ) ,
     )
 
     task_4 = BashOperator(
@@ -34,4 +36,4 @@ with DAG(
         bash_command='sleep 3',
     )
 
-    task_1 >> [task_2, task_3] >> task_4
+    task_1 >> processing >> task_4
