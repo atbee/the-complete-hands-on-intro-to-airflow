0a786d45121690d710337f351e7f00fb49b7c252
atbee <atb@odds.team>
2021-04-28 20:35:20 +0700

    Create tasks group :bee:

---
+++ dags/tasks_group_dag.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.task_group import TaskGroup
+
+from subdags.subdag_parallel_dag import subdag_parallel_dag
+
+default_args = {
+    'start_date': datetime(2021, 1, 1),
+}
+with DAG(
+    'parallel_dag',
+    schedule_interval='@daily',
+    default_args=default_args,
+    concurency=2,
+    max_active_runs=1,
+    catchup=False,
+) as dag:
+    task_1 = BashOperator(
+        task_id='task_1',
+        bash_command='sleep 3',
+    )
+
+    with TaskGroup('processing_tasks') as processing_tasks:
+        task_2 = BashOperator(
+            task_id='task_2',
+            bash_command='sleep 3',
+        ) 
+
+        task_3 = BashOperator( 
+            task_id='task_3',
+            bash_command='sleep 3',
+        )
+
+    task_4 = BashOperator(
+        task_id='task_4',
+        bash_command='sleep 3',
+    )
+
+    task_1 >> processing_tasks >> task_4
+ 
